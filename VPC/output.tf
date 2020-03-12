45b3e96327ac1d0a2fee65648b43abc6c4e5cc97
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-12 18:34:40 -0500

    output files updated

--- VPC/output.tf
+++ VPC/output.tf
@@ -14,14 +14,18 @@ output "Subnet3" {
   value       = "${module.dev.Subnet3}"
 }
 
-output "Subnet1" {
-  value       = "${module.dev.Subnet1}"
+output "Private_Subnet1" {
+  value       = "${module.dev.Private_Subnet1}"
 }
 
-output "Subnet1" {
-  value       = "${module.dev.Subnet1}"
+output "Private_Subnet2" {
+  value       = "${module.dev.Private_Subnet2}"
 }
 
-output "Subnet1" {
-  value       = "${module.dev.Subnet1}"
+output "Private_Subnet3" {
+  value       = "${module.dev.Private_Subnet3}"
+}
+
+output "IGW" {
+  value       = "${module.dev.IGW}"
 }
