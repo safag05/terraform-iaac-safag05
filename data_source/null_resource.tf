3fb8a9efb6a2c171c9320d1f186d8ee192c51c94
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-12 20:42:18 -0500

    added null tf

---
+++ data_source/null_resource.tf
@@ -0,0 +1,26 @@
+resource "null_resource" "safag" {
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+
+    
+    provisioner "remote-exec" {
+    connection {
+        type     = "ssh"
+        user     = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+        host     = "${aws_instance.web.public_ip}"
+    }
+
+
+        inline = [
+           "sudo yum install telnet -y",
+           "sudo mkdir /tmp/centos",
+           "w",
+           "free -m",
+           "sleep 5"
+        ]
+
+    }
+  
+}
