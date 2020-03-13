b6c3574efcbc4f4c6e1caf3bded012ebbeb143c2
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-12 19:18:19 -0500

    updated ec2

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -45,3 +45,13 @@ data "aws_ami" "centos" {
 output "CENTOS_AMI_ID" {
   value = "${data.aws_ami.ubuntu.id}"
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
