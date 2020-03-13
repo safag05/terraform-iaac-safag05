255a4eadcf3dcbbfd0a33a120a55a7a5e89e57b0
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-12 19:39:54 -0500

    updates

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -54,6 +54,7 @@ resource "aws_key_pair" "provisioner" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.provisioner.key_name}"
 
   tags = {
     Name = "HelloWorld"
