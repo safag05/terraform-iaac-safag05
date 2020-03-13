7ebea0bddc06ec39a0404d45ce11e03e5e74d898
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-12 19:14:10 -0500

    addedec2.tf

---
+++ data_source/ec2.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Gets ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+output "UBUNTU_AMI_ID" {
+  value       = "${data.aws_ami.ubuntu.id}"
+}
+
+
+
+
+
+
+
+
+
+# Gets CentOS AMI
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = "CentOS Linux 7 x86_64 HVM EBS ENA 1901_01-b7*"
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+output "CENTOS_AMI_ID" {
+  value       = "${data.aws_ami.ubuntu.id}"
+}
