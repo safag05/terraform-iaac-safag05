3f30689fc967a822d58fd92ee368003a3c5d91dd
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 16:08:58 -0500

    updated Sec group

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -1,5 +1,18 @@
-resource "aws_instance" "web" {
-  ami        = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id = "${data.terraform_remote_state.dev.Subnet1}"
+module "wordpress" {
+  source = "terraform-aws-modules/autoscaling/aws"
+  lc_name = "wordpress-lc"
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_tls.id}"]
+  asg_name                  = "wordpress-asg"
+  vpc_zone_identifier       = [
+                                "${data.terraform_remote_state.dev.Subnet1}",  
+                                "${data.terraform_remote_state.dev.Subnet2}", 
+                                "${data.terraform_remote_state.dev.Subnet3}"
+                              ]
+  health_check_type         = "EC2"
+  min_size                  = 1
+  max_size                  = 5
+  desired_capacity          = 2
+  wait_for_capacity_timeout = 0
 }
