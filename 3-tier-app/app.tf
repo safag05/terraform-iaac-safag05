3456a4f838e91052f9639dfe4e3c57d2e64e5c86
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 15:10:36 -0500

    updates

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "dev" {
   backend = "s3"
   config = {
-    bucket = "backend-state-safag"
-    key    = "tower/us-east-1/tools/virginia/tower.tfstate"
-    region = "us-east-1"
+  bucket = "backend-state-safag"
+  key    = "tower/us-east-1/tools/virginia/tower.tfstate"
+  region = "us-east-1"
   }
 }
