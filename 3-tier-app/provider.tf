ecfb361d32bbe73e1fb01aa404b772774e3bb584
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 16:55:39 -0500

    updated provider under 3 tier

--- 3-tier-app/provider.tf
+++ 3-tier-app/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+
+terraform {
+  required_version = "~> 0.11.14"
+  provider.aws: version = "~> 2.53"
+  provider.null: version = "~> 2.1"
+  provider.random: version = "~> 2.2"
+}
