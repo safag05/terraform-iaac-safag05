841b9aab759ecc362be98f645f171a37af584240
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 15:19:04 -0500

    added output

--- 3-tier-app/output.tf
+++ 3-tier-app/output.tf
@@ -1,3 +1,31 @@
 output "full_list" {
   value       = "${data.terraform_remote_state.dev.*}"
 }
+
+
+
+output "Subnet1" {
+  value       = "${data.terraform_remote_state.dev.Subnet1}"
+}
+
+output "Subnet2" {
+  value       = "${data.terraform_remote_state.dev.Subnet2}"
+}
+
+output "Subnet3" {
+  value       = "${data.terraform_remote_state.dev.Subnet3}" 
+}
+
+
+
+output "Private_Subnet1" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet1}"
+}
+
+output "Private_Subnet2" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet2}"
+}
+
+output "Private_Subnet3" {
+  value       = "${data.terraform_remote_state.dev.Private_Subnet3}" 
+}
