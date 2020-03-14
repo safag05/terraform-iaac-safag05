82296cf362e311da1910a81f9062d24044f76017
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 16:36:33 -0500

    updated ELB

--- 3-tier-app/elb.tf
+++ 3-tier-app/elb.tf
@@ -1,15 +1,13 @@
 resource "aws_elb" "bar" {
   name               = "wordpress"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  access_logs {
-    bucket        = "foo"
-    bucket_prefix = "bar"
-    interval      = 60
-  }
+  subnets    =    [
+                          "${data.terraform_remote_state.dev.Subnet1}",  
+                          "${data.terraform_remote_state.dev.Subnet2}", 
+                          "${data.terraform_remote_state.dev.Subnet3}"
+                  ]
 
   listener {
-    instance_port     = 8000
+    instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
