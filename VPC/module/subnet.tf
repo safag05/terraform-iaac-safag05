74bb5e98a79f13514e9f33da6ab773b69ac5a55c
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-14 14:14:24 -0500

    updated subnet

--- VPC/module/subnet.tf
+++ VPC/module/subnet.tf
@@ -2,16 +2,19 @@
 resource "aws_subnet" "dev1" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block1_public}" 
+  availability_zone = "us-east-1a"
 } 
 
 resource "aws_subnet" "dev2" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block2_public}" 
+  availability_zone = "us-east-1b"
 } 
 
 resource "aws_subnet" "dev3" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block3_public}" 
+  availability_zone = "us-east-1c"
 } 
 
 
